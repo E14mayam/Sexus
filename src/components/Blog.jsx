982480b46a016a39ca208f102b298446edbde065
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-05-21 20:09:34 +0100

    added claps and comments

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,6 +3,8 @@ import placeholderImg from "../assets/tokyo-attention-sign-in-the-web-browser.pn
 import image1 from "../assets/image1.jpg";
 import image2 from "../assets/image2.jpg";
 import image3 from "../assets/image3.jpg";
+import comment from "../icons/message-square.svg"
+import clap from "../icons/clap.png"
 
 const Blog = () => {
   return (
@@ -11,25 +13,37 @@ const Blog = () => {
         <img src={image1} alt={placeholderImg} />
         <h5 className="h5 py-2">Best Kissing Practices</h5>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
-          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+          porro...
         </p>
+        <div className="blog-post-icons d-flex">
+          <h6 className="d-flex">Comment <img src={comment} alt="" /></h6>
+          <h6 className="d-flex">Clap <img src={clap} alt="" /></h6>
+        </div>
       </div>
       <div className="blog-post mx-auto col-lg-4 col-md-6 col-sm-12">
         <img src={image2} alt={placeholderImg} />
         <h5 className="h5 py-2">Best Kissing Practices</h5>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
-          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+          porro...
         </p>
+        <div className="blog-post-icons d-flex">
+          <h6 className="d-flex">Comment <img src={comment} alt="" /></h6>
+          <h6 className="d-flex">Clap <img src={clap} alt="" /></h6>
+        </div>
       </div>
       <div className="blog-post mx-auto col-lg-4 col-md-6 col-sm-12">
         <img src={image3} alt={placeholderImg} />
         <h5 className="h5 py-2">Best Kissing Practices</h5>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
-        
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+          porro...
         </p>
+        <div className="blog-post-icons d-flex">
+          <h6 className="d-flex">Comment <img src={comment} alt="" /></h6>
+          <h6 className="d-flex">Clap <img src={clap} alt="" /></h6>
+        </div>
       </div>
     </div>
   );
