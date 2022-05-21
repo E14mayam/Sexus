6a9b902d41274819002b9767b5fe8619cc5378c5
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-05-21 18:02:32 +0100

    blog updates

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,37 @@
 import React from "react";
-import placeholderImg from "../assets/tokyo-attention-sign-in-the-web-browser.png"
+import placeholderImg from "../assets/tokyo-attention-sign-in-the-web-browser.png";
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
 const Blog = () => {
-    return(
-        <div> <img src={placeholderImg} alt="" className="placeholderImg" /></div>
-    )
-}
+  return (
+    <div className="blog container mx-auto row row-cols-3 mt-5">
+      <div className="blog-post mx-auto col-lg-4 col-md-6 col-sm-12">
+        <img src={image1} alt={placeholderImg} />
+        <h5 className="h5 py-2">Best Kissing Practices</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
+          
+        </p>
+      </div>
+      <div className="blog-post mx-auto col-lg-4 col-md-6 col-sm-12">
+        <img src={image2} alt={placeholderImg} />
+        <h5 className="h5 py-2">Best Kissing Practices</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
+          
+        </p>
+      </div>
+      <div className="blog-post mx-auto col-lg-4 col-md-6 col-sm-12">
+        <img src={image3} alt={placeholderImg} />
+        <h5 className="h5 py-2">Best Kissing Practices</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, porro...
+        
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
