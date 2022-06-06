2a7e4614847d3076a1e565337f9f42027d7673f0
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-06-06 20:38:33 +0100

    form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { auth } from "./firebase";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  email: yup.string().email().required("Enter a registered email"),
+  password: yup.string().min(8).max(20).required("Invalid Password")
+})
 
 const Login = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const {register, handleSubmit, formState: { errors }} = useForm({
+    resolver: yupResolver(schema)
+  })
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -15,23 +21,12 @@ const Login = () => {
     event.preventDefault();
     auth
       .signInWithEmailAndPassword(email, password)
-      .catch((err) => console.log(err));
+      .catch((err) => alert(err.message));
 
     setPassword("");
     setEmail("");
   };
 
-  const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 250,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-  };
 
   return (
     <div className="Upload Signup container mx-auto mb-3">
@@ -42,37 +37,22 @@ const Login = () => {
           name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter a registered email"
+          placeholder="example@gmail.com"
+          ref={register(email, { required: true })}
         />
+         <p>{errors.email?.message}</p>
         <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="example@.gmail.com"
+          placeholder="example123"
+          ref={register(password, { required: true })}
         />
-        <button onClick={signIn && handleOpen}>
-          Login
-        </button>
+        <p>{errors.password?.message}</p>
+        <button onClick={handleSubmit(signIn)}>Login</button>
       </form>
-      <div>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Voila
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              Your Login request was successful
-            </Typography>
-          </Box>
-        </Modal>
-      </div>
     </div>
   );
 };
