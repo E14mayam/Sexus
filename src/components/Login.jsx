60d687f921ac64904454fc7189adb86890c4dc2a
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-06-15 00:36:31 +0100

    updated form validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,59 +1,104 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik } from "formik";
 import { auth } from "./firebase";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 
-const schema = yup.object().shape({
-  email: yup.string().email().required("Enter a recognized email ⓧ"),
-  password: yup.string().min(8).max(20).required("Invalid Password ⓧ")
-})
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validate = (values) => {
+  let errors = {};
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!regex.test(values.email)) {
+    errors.email = "Invalid Email";
+  }
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 4) {
+    errors.password = "Password too short";
+  }
+  return errors;
+};
+
+const submitForm = (values) => {
+  console.log(values);
+  auth
+    .signInWithEmailAndPassword(values.email, values.password)
+    .catch((err) => console.log(err));
+};
 
 const Login = () => {
-  const {register, handleSubmit, formState: { errors }} = useForm({
-    resolver: yupResolver(schema)
-  })
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const signIn = (event) => {
-    event.preventDefault();
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .catch((err) => alert(err.message));
-
-    setPassword("");
-    setEmail("");
-  };
-
-
   return (
-    <div className="Upload Signup container mx-auto mb-3">
-      <form className="d-flex flex-column px-5">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="example@gmail.com"
-          ref={register(email, { required: true })}
-        />
-         <p>{errors.email?.message}</p>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="example123"
-          ref={register(password, { required: true })}
-        />
-        <p>{errors.password?.message}</p>
-        <button onClick={handleSubmit(signIn)}>Login</button>
-      </form>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={submitForm}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty,
+        } = formik;
+        return (
+          <div className="Upload Signup container mx-auto mb-3">
+            <form className="d-flex flex-column px-5" onSubmit={handleSubmit}>
+              <div className="form-row d-flex flex-column">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email ? "input-error" : null
+                  }
+                />
+                {errors.email && touched.email && (
+                  <span className="error">{errors.email}</span>
+                )}
+              </div>
+
+              <div className="form-row d-flex flex-column">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : null
+                  }
+                />
+                {errors.password && touched.password && (
+                  <span className="error">{errors.password}</span>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className={dirty && isValid ? "" : "disabled-btn"}
+                disabled={!(dirty && isValid)}
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        );
+      }}
+    </Formik>
   );
 };
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,110 +1,155 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "./firebase";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
+import { Formik } from "formik";
 
+const initialValues = {
+  username: "",
+  email: "",
+  password: ""
+};
 
-const schema = yup.object().shape({
-  email: yup.string().email().required("Enter an email ⓧ"),
-  password: yup.string().min(8).max(20).required("Invalid Password ⓧ"),
-  username: yup.string().min(3).max(10).matches(/^[aA-zZ\s]+$/, "Only lower-case alphabets are allowed for this field ⓧ").required("Username must be between 3 and 10 characters ⓧ")
-})
+const validate = (values) => {
+  let errors = {};
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  const usernamevalid = /^[aA-zZ\s]+$/;
+  if (!values.username) {
+    errors.username = "Enter a username";
+  } else if (!usernamevalid.test(values.username)) {
+    errors.username = "Invalid Username";
+  }
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!regex.test(values.email)) {
+    errors.email = "Invalid Email";
+  }
+  if (!values.password) {
+    errors.password = "Password is required";
+  } else if (values.password.length < 4) {
+    errors.password = "Password too short";
+  }
+  return errors;
+};
+
+const submitForm = (values) => {
+  auth
+    .createUserWithEmailAndPassword(values.email, values.password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: values.username,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 const SignUp = () => {
-  const {register, handleSubmit, formState: { errors }, reset} = useForm({
-    resolver: yupResolver(schema),
-    reValidateMode:"onBlur" 
-  })
-
-
-  const [username, setUsername] = useState("");
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      if (authUser) {
-        //user has logged in
-        setUser(authUser);
-        if (authUser.displayName) {
-          //don't update
-        } else {
-          //create new user
-          return authUser.updateProfile({
-            displayName: username,
-          });
-        }
-      } else {
-        //user has logged out
-        setUser(null);
-      }
-    });
-    return () => {
-      //perform cleanup actions
-      unsubscribe();
-    };
-  }, [user, username]);
-
-  const signUpUser = (event) => {
-    console.log('fish')
-    event.preventDefault();
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((authUser) => {
-        return authUser.user.updateProfile({
-          displayName: username,
-        });
-      })
-      .catch((err) => console.log(err));
-
-    setPassword("");
-    setEmail("");
-    setUsername("");
-    reset();
-  };
-
   return (
-    <div className="Upload Signup container mx-auto mb-3" >
-      <form className="d-flex flex-column px-5">
-        <label htmlFor="userName">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter a username"
-          inputref={register(username)}
-          required
-        />
-        <p>{errors.username?.message} </p>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="example@.gmail.com"
-          inputref={register(email)}
-          required
-        />
-        <p>{errors.email?.message}</p>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter a password"
-          inputref={register(password)}
-          required
-        />
-        <p>{errors.password?.message}</p>
-        <button onClick={handleSubmit(signUpUser)}>SignUp</button>
-      </form>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={submitForm}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+          isValid,
+          dirty,
+        } = formik;
+        return (
+          <div className="Upload Signup container mx-auto mb-3">
+            <form
+              className="d-flex flex-column px-5"
+              onSubmit={handleSubmit}
+            >
+              <div className="form-row d-flex flex-column">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="username"
+                  name="username"
+                  id="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.username && touched.username ? "input-error" : null
+                  }
+                />
+                {errors.username && touched.username && (
+                  <span className="error">{errors.username}</span>
+                )}
+              </div>
+
+              <div className="form-row d-flex flex-column">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email ? "input-error" : null
+                  }
+                />
+                {errors.email && touched.email && (
+                  <span className="error">{errors.email}</span>
+                )}
+              </div>
+
+              <div className="form-row d-flex flex-column">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.password && touched.password ? "input-error" : null
+                  }
+                />
+                {errors.password && touched.password && (
+                  <span className="error">{errors.password}</span>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className={dirty && isValid ? "" : "disabled-btn"}
+                disabled={!(dirty && isValid)}
+              >
+                Signup
+              </button>
+            </form>
+          </div>
+        );
+      }}
+    </Formik>
   );
 };
+
+// const schema = yup.object().shape({
+//   email: yup.string().email().required("Enter an email ⓧ"),
+//   password: yup.string().min(8).max(20).required("Invalid Password ⓧ"),
+//   username: yup.string().min(3).max(10).matches(/^[aA-zZ\s]+$/, "Only lower-case alphabets are allowed for this field ⓧ").required("Username must be between 3 and 10 characters ⓧ")
+// })
+
+// const signUpUser = (event) => {
+//   event.preventDefault();
+//   auth
+//     .createUserWithEmailAndPassword(email, password)
+//     .then((authUser) => {
+//       return authUser.user.updateProfile({
+//         displayName: username,
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// };
 
 export default SignUp;
